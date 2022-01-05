7a9bd03b4d60486b79a173c1a8cb62e30297df9f
likaiping <2111289196@qq.com>
2022-01-05 19:00:16 +0800

    master commit conflix

--- src/main/java/com/lkp/git/Hello.java
+++ src/main/java/com/lkp/git/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hot-fix");
+        System.out.println("master-conflix");
     }
 }
